6567a4cea53d216e6aaf4dfce90de7b06c8c0309
l08084 <l08084.1989@gmail.com>
2021-07-03 19:26:33 +0900

    fix testcode

--- tests/rules/click-events-have-tappable.test.ts
+++ tests/rules/click-events-have-tappable.test.ts
@@ -6,10 +6,20 @@ const tester = new TSESLint.RuleTester({
 });
 
 tester.run('haveTappable', haveTappable, {
-  valid: [{ code: '<h1>Heading Content!</h1>' }],
+  valid: [
+    { code: `<h1>Heading Content!</h1>` },
+    { code: `<div tappable (click)="doClick()">I am clickable!</div>` },
+    { code: `<button (click)="doClick()">I am clickable!</button>` },
+    { code: `<ion-button (click)="doClick()">I am clickable!</ion-button>` },
+    { code: `<a (click)="doClick()">I am clickable!</a>` },
+  ],
   invalid: [
     {
-      code: '<h1>Heading Content!</h1>',
+      code: `<div (click)="doClick()">I am clickable!</div>`,
+      errors: [{ messageId: 'haveTappable' }],
+    },
+    {
+      code: `<span (click)="doClick()">I am clickable!</span>`,
       errors: [{ messageId: 'haveTappable' }],
     },
   ],
