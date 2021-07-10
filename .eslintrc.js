b265e42ce171e7092a462c45193a33d5fcbdada4
l08084 <l08084.1989@gmail.com>
2021-07-10 18:46:43 +0900

    delete rulesdir

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'rulesdir'],
+  plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {},
 };
